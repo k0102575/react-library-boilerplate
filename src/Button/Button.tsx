17fdf5dd471ef872122a30bf736d9c2984034fbf
k0102575 <k0102575@gmail.com>
2023-03-09 21:12:16 +0900

    feat: setting emotion & button css

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -1,15 +1,34 @@
+/** @jsxImportSource @emotion/react */
 import { ReactNode } from 'react';
+import { css } from '@emotion/react';
 
 /*
   Interface
 */
 export interface ButtonProps {
-  children: ReactNode;
+  className?: string;
+  children?: ReactNode;
   onClick?: () => void;
 }
 
-const Button = ({ children = "Button", onClick }: ButtonProps) => {
-  return <div onClick={onClick}>{children}</div>;
+/*
+  Style
+*/
+const ButtonStyle = () => css`
+  width: 500px;
+  height: 50px;
+  background-color: lightblue;
+`;
+
+/*
+  Component
+*/
+const Button = ({ className, children = 'Button', onClick }: ButtonProps) => {
+  return (
+    <button className={className} css={ButtonStyle} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
